0090fa5a796d296b194453a13eb949c0fbbce46a
1Sam Online <csh@korea.com>
2015-02-21 19:03:40 +0900

    Update popup.js

--- tpl/popup.js
+++ tpl/popup.js
@@ -1,41 +1,38 @@
 /**
  * popup으로 열렸을 경우 부모창의 위지윅에디터에 select된 멀티미디어 컴포넌트 코드를 체크하여 있으면 가져와서 원하는 곳에 삽입
  
- ////* 옵션 추가 메뉴얼////
+ // 옵션 추가 메뉴얼
  
-	 |* 옵션의 정의는 아래의 3개의 파일에 연결됩니다.
-	 |* popup.html 에서는 id="변수"
-	 |* popup.js 에서는 아래의 4단계를 참고하세요.
-	 |* multimedia_link.class.php에는 $xml_obj->attrs->변수 형태 사용합니다.
- 
-	 |* 옵션으로 사용할 변수의 추가는 모두 4단계입니다. 1/4,2/4,3/4,4/4 단계로 변수를 추가하세요.
-/////////////////////////// 
+ 옵션의 정의는 아래의 3개의 파일에 연결됩니다.
+ popup.html 에서는 id="변수"
+ popup.js 에서는 아래의 4단계를 참고하세요.
+ multimedia_link.class.php에는 $xml_obj->attrs->변수 형태 사용합니다.
+
+ 옵션으로 사용할 변수의 추가는 모두 4단계입니다. 1/4,2/4,3/4,4/4 단계로 변수를 추가하세요.
  **/
 
-jQuery(function($){
+//창 열었을 때, 초기값 설정
+jQuery(function ($) {
+	if(!is_def(opener)) return;
 
-	//창 열었을 때, 초기값 설정
-	(function(){
-		if(!is_def(opener)) return;
-	
-		var $node = $(opener.editorPrevNode).filter('img'), attrs;
-		if(!$node.length) return;
+	var $node = $(opener.editorPrevNode).filter('img'), attrs;
+	if(!$node.length) return;
 
 // 변수의 기본값
-		attrs = {
-			url     : $node.attr('multimedia_src') || null,
-			caption : $node.attr('alt') || null,
-			width   : $node.width() || '896',
-			height  : $node.height() || '504',
-			wmode   : $node.attr('wmode') || null
-		};
-	
-		$.each(attrs, function(key, val) {
-			get_by_id('multimedia_'+key).value = val;
-		});
-	
-		get_by_id('auto_start').checked = ($node.attr('auto_start') == 'true');
-		get_by_id('multimedia_responsive').checked = ($node.attr('multimedia_responsive') == 'true');
+	attrs = {
+		url     : $node.attr('multimedia_src') || null,
+		caption : $node.attr('alt') || null,
+		width   : $node.width() || '896',
+		height  : $node.height() || '504',
+		wmode   : $node.attr('wmode') || null
+	};
+
+	$.each(attrs, function (key, val) {
+		get_by_id('multimedia_'+key).value = val;
+	});
+
+	get_by_id('auto_start').checked = ($node.attr('auto_start') == 'true');
+	get_by_id('multimedia_responsive').checked = ($node.attr('multimedia_responsive') == 'true');
 
 // 1/4. 변수 추가
 // 모두 4항목에 알맞은 형태로 추가해야합니다.
@@ -43,14 +40,11 @@ jQuery(function($){
 // $xml_obj->attrs->변수
 // 형태로 넘겨집니다.
 // 변수의 저장된 값을 팝업창에 표시합니다.
-		get_by_id('yt_loop').checked = ($node.attr('yt_loop') || '0');
-		get_by_id('volume').value = ($node.attr('volume') || '70');
-
-	})();
-
+	get_by_id('yt_loop').checked = ($node.attr('yt_loop') || '0');
+	get_by_id('volume').value = ($node.attr('volume') || '70');
 
 	// 완료 버튼 클릭시
-	$('.btnArea button').click(function(){
+	$('.btnArea button').click(function () {
 		if(!is_def(opener)) return;
 	
 		var el_wmode = get_by_id('fo').elements['multimedia_wmode'];
@@ -62,61 +56,52 @@ jQuery(function($){
 			auto_start : get_by_id('auto_start').checked?'true':'false',
 			multimedia_src : get_by_id('multimedia_url').value.replace(request_uri, ''),
 			multimedia_responsive : get_by_id('multimedia_responsive').checked?'true':'false',
-
-// 2/4. 변수 추가
-// 변수의 값을 정렬합니다..
+	
+	// 2/4. 변수 추가
+	// 변수의 값을 정렬합니다..
 			yt_loop : get_by_id('yt_loop').checked?'1':'0',
 			volume : get_by_id('volume').value || 70
-	    };
-
-
-    	if(!attrs['multimedia_src']) {
-    	  window.close();
-    	  return;
-    	}
-
-    	var $selected_node = $(opener.editorPrevNode);
-
-    	if($selected_node.is('img') && $selected_node.attr('editor_component') == 'multimedia_link'){
-    		$selected_node
-    			.attr('multimedia_src', attrs.multimedia_src)
-    			.attr('width', attrs.width)
-    			.attr('height', attrs.height)
-    			.attr('wmode', attrs.wmode)
-    			.attr('auto_start', attrs.auto_start)
-    			.attr('alt', attrs.alt)
-    			.attr('multimedia_responsive', attrs.multimedia_responsive)
-// 3/4. 변수 추가
-// 2/4에서 정렬된 실제 저장값을 변수에 담습니다.
-    			.attr('yt_loop', attrs.yt_loop)
-    			.attr('volume', attrs.volume)
-
-    			.css('width', attrs.width + 'px')
-    			.css('height', attrs.height + 'px')
-
-
-    
-    	} else {
-			
-// 4/4. 변수 추가 마지막 과정
-// <img 태그 안에 
-// 변수=""
-// 형태로 꼭 추가합니다. 따옴포 안에 값은 넣지 않습니다.
-    		var html = '<img src="../../../../common/img/blank.gif" editor_component="multimedia_link" multimedia_src="" width="" height="" wmode="" style="display:block;width:'+attrs.width+'px;height:'+attrs.height+'px;border:2px dotted #4371B9;background:url(./modules/editor/components/multimedia_link/tpl/multimedia_link_component.gif) no-repeat center" auto_start="" alt="" multimedia_responsive="" yt_loop="" volume="" />';
-    
-    		html = html.replace(/(\w+)=""/g, function(m0,m1) {
-    			return attrs[m1] ? (m1+'="'+attrs[m1]+'"') : '';
-    		});
-    
-    		opener.editorFocus(opener.editorPrevSrl);
-    
-    		var iframe_obj = opener.editorGetIFrame(opener.editorPrevSrl)
-    
-    		opener.editorReplaceHTML(iframe_obj, html);
-    	}
-    	opener.editorFocus(opener.editorPrevSrl);
-    
-    	window.close();
-    });
-
-});
+		};
+	
+		if(!attrs['multimedia_src']) {
+		  window.close();
+		  return;
+		}
+	
+		var $selected_node = $(opener.editorPrevNode);
+	
+		if($selected_node.is('img') && $selected_node.attr('editor_component') == 'multimedia_link'){
+			$selected_node
+				.attr('multimedia_src', attrs.multimedia_src)
+				.attr('width', attrs.width)
+				.attr('height', attrs.height)
+				.attr('wmode', attrs.wmode)
+				.attr('auto_start', attrs.auto_start)
+				.attr('alt', attrs.alt)
+				.attr('multimedia_responsive', attrs.multimedia_responsive)
+	// 3/4. 변수 추가
+	// 2/4에서 정렬된 실제 저장값을 변수에 담습니다.
+				.attr('yt_loop', attrs.yt_loop)
+				.attr('volume', attrs.volume)
+	
+				.css('width', attrs.width + 'px')
+				.css('height', attrs.height + 'px')
+		} else {
+	// 4/4. 변수 추가 마지막 과정
+	// <img 태그 안에 
+	// 변수=""
+	// 형태로 꼭 추가합니다. 따옴포 안에 값은 넣지 않습니다.
+			var html = '<img src="../../../../common/img/blank.gif" editor_component="multimedia_link" multimedia_src="" width="" height="" wmode="" style="display:block;width:'+attrs.width+'px;height:'+attrs.height+'px;border:2px dotted #4371B9;background:url(./modules/editor/components/multimedia_link/tpl/multimedia_link_component.gif) no-repeat center" auto_start="" alt="" multimedia_responsive="" yt_loop="" volume="" />';
+	
+			html = html.replace(/(\w+)=""/g, function(m0,m1) {
+				return attrs[m1] ? (m1+'="'+attrs[m1]+'"') : '';
+			});
+	
+			opener.editorFocus(opener.editorPrevSrl);
+			var iframe_obj = opener.editorGetIFrame(opener.editorPrevSrl)
+			opener.editorReplaceHTML(iframe_obj, html);
+		}
+		opener.editorFocus(opener.editorPrevSrl);
+		window.close();
+	});
+})(jQuery);
